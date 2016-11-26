d5a8e6075fbd72505f4998b64661e37b2484f75f
ljh <ljinghang@yeah.nt>
2016-11-26 23:48:24 +0800

    从C中调用汇编

--- src/kernel/init.c
+++ src/kernel/init.c
@@ -1,5 +1,8 @@
 
-void init(){
+void write_mem8(int addr, int data);
+
+void init(void){
     char *show = 0xb8010;
     *show = 'B';
+    write_mem8((int)0xb8012,(int)'C');
 }
--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -1,6 +1,8 @@
 [bits 32]
 extern init
 
+global write_mem8
+
 start:
 mov edi,0
 mov ah,0CH
@@ -8,5 +10,13 @@ mov al,'K'
 mov [gs:edi],ax
 
 call init
+hlt
 
-jmp$
+;
+write_mem8:     ;void write_mem8(int addr, int data);
+mov ecx,[esp+4] ;第一个参数
+mov al,[esp+8]  ;第二个参数
+mov [gs:ecx],al
+ret
+
+jmp $
