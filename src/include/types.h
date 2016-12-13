83f2d5a55128e705370e79f91a1009855ab998c3
ljh <ljinghang@yeah.net>
2016-12-13 17:01:28 +0800

    类型头文件

---
+++ src/include/types.h
@@ -0,0 +1,20 @@
+#ifndef _INCLUDE_TYPES_H_
+#define _INCLUDE_TYPES_H_
+
+#ifndef NULL
+    #define NULL 0
+#endif
+
+#ifndef TRUE
+    #define TRUE 1
+    #define FALSE 0
+#endif
+
+typedef enum bool{
+    false = 0,
+    true = 1
+} bool;
+
+
+
+#endif //_INCLUDE_TYPES_H_
