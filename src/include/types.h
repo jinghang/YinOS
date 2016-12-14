38b57fa29855d297db614e85ba0e096370f86384
ljh <ljinghang@yeah.net>
2016-12-14 09:46:26 +0800

    gcc 内嵌汇编

--- src/include/types.h
+++ src/include/types.h
@@ -15,6 +15,13 @@ typedef enum bool{
     true = 1
 } bool;
 
-
+typedef unsigned    int     uint32_t;
+typedef             int     int32_t;
+typedef unsigned    short   uint16_t;
+typedef             short   int16_t;
+typedef unsigned    char    uint8_t;
+typedef             char    int8_t;
+typedef unsigned    int     size_t;
+typedef             int     ssize_t;
 
 #endif //_INCLUDE_TYPES_H_
--- src/kernel/init.c
+++ src/kernel/init.c
@@ -1,7 +1,14 @@
 void write_mem8(unsigned int addr, char c);
+#include <types.h>
+
+//写内存
+void write_mem(void * dest, uint8_t c){
+    __asm__ __volatile__ ("movb %1,(%0)"::"r"(dest),"r"(c));
+}
 
 void init(void){
     char *show = (char*)0xb8010;
     *show = 'B';
     write_mem8(0xb8012,'C');
+    write_mem(0xb8014,'D');
 }
